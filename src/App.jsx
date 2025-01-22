eb5af54538052e201f0b2212d7da0e1651c3f711
Sawjal-sikder <mdsajalsikder@gmail.com>
2025-01-22 07:22:46 +0600

    Navbar setting in this project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import ProductList from './assets/Component/Product/ProductList'
+import Navbar from './assets/Component/Header/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>List</h1>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' Component={ ProductList } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/assets/Component/Header/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="bg-slate-600 flex justify-between items-center p-4 text-white">
+            <Link to={'/'} className='text-3xl ps-24 font-medium'> Logo </Link>
+          <ul id="nav-mobile" className="flex space-x-4">
+            <Link to={'/'} className=''> Product </Link>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
