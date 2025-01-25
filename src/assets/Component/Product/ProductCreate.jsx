7697f9e44b7b840bc2b890773ee0b2760ae6606a
Sawjal_sikder <mdsajalsikder@gmail.com>
2025-01-25 16:43:21 +0600

    create product to live server

--- src/assets/Component/Product/ProductCreate.jsx
+++ src/assets/Component/Product/ProductCreate.jsx
@@ -1,29 +1,59 @@
-import React from 'react'
-import Create from '../../Hooks/Create'
+import React from 'react';
+import Create from '../../Hooks/Create';
 
 function ProductCreate() {
-    const url = 'http://127.0.0.1:8000/product/'
-    const initialData = {title:'', price:''};
-    const {data, setData, CreateData} = Create(url, initialData);
+  const url = 'https://api.nmmbd.com/product/';
+  const initialData = { title: '',description:'', price: ''};
+  const { data, setData, CreateData } = Create(url, initialData);
 
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        CreateData();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await CreateData();
+  };
 
-    }
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setData((prevData) => ({ ...prevData, [name]: value }));
-    };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name='title' placeholder='Enter Product Titele Here...' onChange={handleChange}  value={data.title}/>
-      <input type="text" name='price' placeholder='Enter Product Price Here...' onChange={handleChange}  value={data.price} />
-      <button type="submit">Submite</button>
+    <form onSubmit={handleSubmit} encType="multipart/form-data">
+      <div>
+        <label htmlFor="title">Product Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Enter Product Title Here..."
+          onChange={handleChange}
+          value={data.title}
+        />
+      </div>
+      <div>
+        <label htmlFor="description">Product description</label>
+        <textarea
+          id="description"
+          name="description"
+          placeholder="Enter Product Price Here..."
+          onChange={handleChange}
+          value={data.description}
+        >
+        </textarea>      
+      </div>
+      <div>
+        <label htmlFor="price">Product Price</label>
+        <input
+          type="text"
+          id="price"
+          name="price"
+          placeholder="Enter Product Price Here..."
+          onChange={handleChange}
+          value={data.price}
+        />
+      </div>
+      <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
-export default ProductCreate
+export default ProductCreate;
--- src/assets/Hooks/Create.jsx
+++ src/assets/Hooks/Create.jsx
@@ -1,18 +1,21 @@
-import axios from 'axios';
-import React, { useState } from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 function Create(url, initialData) {
-    const [data, setData] =useState(initialData);
+  const [data, setData] = useState(initialData);
 
-    const CreateData = async () =>{
-        try {
-            const res = await axios.post(url, data);
-            setData(res.data);
-            setData(initialData);
-        } catch (error) {
-            console.log(error);
-        }
+  const CreateData = async () => {
+    try {
+      const res = await axios.post(url, data);
+      setData(res.data);
+      setData(initialData);
+    } catch (err) {
+      console.log(err.message); 
     }
-  return {data, setData, CreateData}
+  };
+
+  return { data, setData, CreateData };
 }
-export default Create
+
+export default Create;
+
